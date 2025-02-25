ab71ca5296681840eeca5e50cd8dd267a8c64244
shaunsteele <shaun.steele.1020@gmail.com>
2025-02-24 23:00:38 -0800

    added to sequence item, built monitor bfm, changed sequencing

--- sim/tb_axi_lite_mm.py
+++ sim/tb_axi_lite_mm.py
@@ -5,13 +5,7 @@ from cocotb.triggers import ClockCycles
 import pyuvm
 from pyuvm import uvm_test, ConfigDB, uvm_sequence, s14_15_python_sequences
 
-from vip_axi_lite import AxiLiteBfm, AxiLiteAgent, AddressWriteSeq
-
-
-class VirtualSequence(uvm_sequence):
-    async def body(self):
-        seq = AddressWriteSeq("seq")
-        await seq.start()
+from vip_axi_lite import AxiLiteBfm, AxiLiteAgent, WriteSeq
 
 
 @pyuvm.test()
@@ -25,7 +19,9 @@ class AddressWriteTest(uvm_test):
 
     async def run_phase(self):
         self.raise_objection()
-        self.seq = VirtualSequence("seq")
+        sqr = ConfigDB().get(self, "", "AXI_LITE_SQR")
+        self.seq = WriteSeq("seq")
+        await self.seq.start(sqr)
         await ClockCycles(self.bfm.clk, 100)
         self.drop_objection()
 
--- sim/vip_axi_lite.py
+++ sim/vip_axi_lite.py
@@ -1,7 +1,7 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.queue import Queue, QueueEmpty
-from cocotb.triggers import ClockCycles, RisingEdge, FallingEdge, First
+from cocotb.triggers import ClockCycles, RisingEdge, FallingEdge, Combine, First
 
 from pyuvm import utility_classes, UVMError
 from pyuvm import uvm_sequence_item
@@ -12,9 +12,93 @@ from pyuvm import uvm_subscriber
 from pyuvm import uvm_agent, uvm_sequencer
 
 import vsc
+import warnings
 from enum import Enum
 
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+# transaction
+class Cmd(Enum):
+    READ = 0
+    WRITE = 1
+
+@vsc.randobj
+class AxiLiteSeqItem(uvm_sequence_item):
+    def __init__(self, name):
+        super().__init__(name)
+        self.cmd = vsc.rand_enum_t(Cmd)
+        self.awdelay = vsc.rand_bit_t(2)
+        self.wdelay = vsc.rand_bit_t(2)
+        self.bdelay = vsc.rand_bit_t(2)
+        self.addr = vsc.rand_uint64_t()
+        self.prot = vsc.rand_bit_t(3)
+        self.data = vsc.rand_uint64_t()
+        self.wstrb = vsc.rand_uint8_t()
+        self.resp = vsc.rand_bit_t(2)
+
+    @vsc.constraint
+    def cmd_c(self):
+        self.cmd == Cmd.WRITE
+    
+    @vsc.constraint
+    def addr_c(self):
+        self.addr < 0x1000
+
+    @vsc.constraint
+    def prot_c(self):
+        self.prot == 0b000
+
+    @vsc.constraint
+    def strb_c(self):
+        self.wstrb == 0xFF
+
+    @vsc.constraint
+    def resp_c(self):
+        self.resp == 0
+
+    def copy_addr(self, rhs):
+        self.addr = rhs.addr
+    
+    def copy_prot(self, rhs):
+        self.prot = rhs.prot
+    
+    def copy_data(self, rhs):
+        self.data = rhs.data
+
+    def copy_wstrb(self, rhs):
+        self.wstrb = rhs.wstrb
+    
+    def copy_resp(self, rhs):
+        self.resp = rhs.resp
+
+    def __eq__(self, rhs):
+        if self.cmd != rhs.cmd:
+            return False
+        if self.addr != rhs.addr and self.prot != rhs.prot:
+            return False
+        if self.data != rhs.data and self.resp != rhs.resp:
+            return False
+        return True
+            
+    def __str__(self):
+        s = f"{self.get_type_name()}: {self.cmd.name}\n"
+        s = f"{s}\t{self.prot:#05b}\t{self.addr:#010x}:"
+        s = f"{s}\t{self.data:#010x}\t{self.resp:#04b}\n"
+        s = f"{s}\tawdelay: {self.awdelay}\twdelay: {self.wdelay}"
+        return s
+
+
+# sequence library
+class WriteSeq(uvm_sequence):
+    async def body(self):
+        item = AxiLiteSeqItem("item")
+        await self.start_item(item)
+        item.randomize()
+        item.kind = Cmd.WRITE
+        await self.finish_item(item)
+
+
+# agent
 class AxiLiteBfm(metaclass=utility_classes.Singleton):
     def __init__(self, reset_length=20):
         self.dut = cocotb.top
@@ -24,7 +108,7 @@ class AxiLiteBfm(metaclass=utility_classes.Singleton):
         self.driver_queue = Queue()
         self.monitor_queue = Queue()
 
-    def put_driver_queue(self, item):
+    async def put_driver_queue(self, item):
         self.driver_queue.put_nowait(item)
 
     async def initialize(self):
@@ -33,73 +117,125 @@ class AxiLiteBfm(metaclass=utility_classes.Singleton):
         self.dut.i_axi_awvalid.value = 0
         self.dut.i_axi_awaddr.value = 0
         self.dut.i_axi_awprot.value = 0
+        self.dut.i_axi_wvalid.value = 0
+        self.dut.i_axi_wdata.value = 0
+        self.dut.i_axi_wstrb.value = 0
+        self.dut.i_axi_bready.value = 0
         await ClockCycles(self.clk, self.reset_length)
         self.dut.aresetn.value = 1
 
     async def drive(self):
-        await RisingEdge(self.clk)
-        try:
-            self.item = self.driver_queue.get_nowait()
-            await self._drive_write(self.item)
-            # todo: add _drive_read(self.item) and Combine
-        except QueueEmpty:
-            pass
+        while True:
+            await RisingEdge(self.clk)
+            try:
+                item = self.driver_queue.get_nowait()
+                await self._drive_write(item)
+                # todo: add _drive_read(self.item) and Combine
+            except QueueEmpty:
+                pass
 
     async def _drive_write(self, item):
-        await self._drive_aw(item)
+        await Combine(
+            cocotb.start_soon(self._drive_aw(item)),
+            cocotb.start_soon(self._drive_w(item))
+        )
+        await self._drive_b(item)
+
+    async def _wait_for_signal(self, signal, level=1,timeout=100):
+        ct = 0
+        while int(signal.value) != level and ct < timeout:
+            await FallingEdge(self.clk)
+            ct += 1
+        if ct >= timeout:
+            raise TimeoutError()
 
     async def _drive_aw(self, item):
-        if (item.kind == Cmd.WRITE):
+        if item.kind == Cmd.WRITE:
             await ClockCycles(self.clk, item.awdelay)
             self.dut.i_axi_awvalid.value = 1
-            self.dut.i_axi_awaddr.value = item.waddr
-            self.dut.i_axi_awprot.value = item.wprot
+            self.dut.i_axi_awaddr.value = item.addr
+            self.dut.i_axi_awprot.value = item.prot
 
             await FallingEdge(self.clk)
-            ready = cocotb.start_soon(await self.dut.o_axi_awready)
-            timeout = cocotb.start_soon(await ClockCycles(self.clk, 100))
-            result = First(ready, timeout)
-            if result is timeout.complete:
-                raise UVMError("Drive AW channel timeout")
+            await self._wait_for_signal(self.dut.o_axi_awready)
         else:
             await RisingEdge(self.clk)
 
+    async def _drive_w(self, item):
+        if item.kind == Cmd.WRITE:
+            await ClockCycles(self.clk, item.wdelay)
+            self.dut.i_axi_wvalid.value = 1
+            self.dut.i_axi_wdata.value = item.data
+            self.dut.i_axi_wstrb.value = item.wstrb
+
+            await FallingEdge(self.clk)
+            await self._wait_for_signal(self.dut.o_axi_wready)
+        else:
+            await RisingEdge(self.clk)
+
+    async def _drive_b(self, item):
+        if item.kind == Cmd.WRITE:
+            await ClockCycles(self.clk, item.bdelay)
+            self.dut.i_axi_bready.value = 1
+
+            await FallingEdge(self.clk)
+            await self._wait_for_signal(self.dut.o_axi_bvalid)
+        else:
+            await RisingEdge(self.clk)
+
+    async def monitor(self):
+        while True:
+            w = cocotb.start_soon(self._monitor_write())
+            result = await First(w)
+            cocotb.log.info(w.done())
+            cocotb.log.info("monitor")
+            if result is w.done():
+                self.monitor_queue.put_nowait(w.result())
+                cocotb.log.info(f"monitor\n{w.result()}")
+
+    async def _monitor_write(self):
+        mon_aw_task = cocotb.start_soon(self._monitor_aw())
+        mon_w_task = cocotb.start_soon(self._monitor_w())
+        mon_b_task = cocotb.start_soon(self._monitor_b())
+        await Combine(mon_aw_task, mon_w_task, mon_b_task)
+        item = AxiLiteSeqItem("item")
+        item.cmd = Cmd.WRITE
+        item.copy_addr(mon_aw_task.result())
+        item.copy_prot(mon_aw_task.result())
+        item.copy_data(mon_w_task.result())
+        item.copy_wstrb(mon_w_task.result())
+        item.copy_resp(mon_b_task.result())
+        return item
+
+    async def _monitor_aw(self):
+        while not(int(self.dut.i_axi_awvalid.value) and int(self.dut.o_axi_awready.value)):
+            await FallingEdge(self.clk)
+        item = AxiLiteSeqItem("item")
+        item.addr = int(self.dut.i_axi_awaddr.value)
+        item.prot = int(self.dut.i_axi_awprot.value)
+        cocotb.log.info(f"_monitor_aw:\t{item.addr:#010x}\t{item.prot:#05b}")
+        return item
+
+    async def _monitor_w(self):
+        while not(int(self.dut.i_axi_wvalid.value) and int(self.dut.o_axi_wready.value)):
+            await FallingEdge(self.clk)
+        item = AxiLiteSeqItem("item")
+        item.data = int(self.dut.i_axi_wdata.value)
+        item.wstrb = int(self.dut.i_axi_wstrb.value)
+        cocotb.log.info(f"_monitor_w:\t{item.data:#010x}\t{item.wstrb:#04x}")
+        return item
+    
+    async def _monitor_b(self):
+        while not(int(self.dut.o_axi_bvalid.value) and int(self.dut.i_axi_bready.value)):
+            await FallingEdge(self.clk)
+        item = AxiLiteSeqItem("item")
+        item.resp = int(self.dut.o_axi_bresp.value)
+        cocotb.log.info(f"_monitor_b:\t{item.resp:#04b}")
+        return item
+
     def start_bfm(self):
         cocotb.start_soon(self.drive())
-
-
-class Cmd(Enum):
-    READ = 0
-    WRITE = 1
-
-@vsc.randobj
-class AddressWriteSeqItem(uvm_sequence_item):
-    def __init__(self, name):
-        super().__init__(name)
-        self.cmd = vsc.rand_enum_t(Cmd)
-        self.awdelay = vsc.rand_bit_t(2)
-        self.waddr = vsc.rand_uint64_t()
-        self.wprot = vsc.rand_bit_t(3)
-
-    def __eq__(self, rhs):
-        return self.cmd == rhs.cmd and self.waddr == rhs.waddr and \
-            self.wprot == rhs.wprot
-    
-    def __str__(self):
-        s = f"{self.get_name()}: {self.cmd.name}\n"
-        if (self.cmd == Cmd.WRITE):
-            s = f"{s}\t{self.waddr:#010X}:\t{self.wdata:#010x}"
-            s = f"{s}\tprot:\t{self.wprot:#05b}"
-        return s
-
-
-class AddressWriteSeq(uvm_sequence):
-    async def body(self):
-        item = AddressWriteSeqItem("item")
-        await self.start_item(item)
-        item.randomize()
-        item.kind = Cmd.WRITE
-        await self.finish_item(item)
+        cocotb.start_soon(self.monitor())
 
 
 class AxiLiteDriver(uvm_driver):
@@ -110,7 +246,7 @@ class AxiLiteDriver(uvm_driver):
         await self.bfm.initialize()
         while True:
             item = await self.seq_item_port.get_next_item()
-            self.bfm.put_driver_queue(item)
+            await self.bfm.put_driver_queue(item)
             self.seq_item_port.item_done()
 
 
